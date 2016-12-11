6da12c9c6794e7f39246fd49537c9fb8d77de80b
Cindy Soares <cindy.as@gmail.com>
2016-12-11 19:31:23 -0200

    configuring Spring Boot

--- src/main/java/com/visiansystems/RateConverter.java
+++ src/main/java/com/visiansystems/RateConverter.java
@@ -6,14 +6,28 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.visiansystems.bl.bankRateFeed.BankRateFeed;
+import com.visiansystems.exception.BankRateFeedException;
+
+@Component
 @Path("/convert")
 @Produces(MediaType.APPLICATION_JSON)
 public class RateConverter {
+	
+
+	@Autowired
+    @Qualifier("bcbRateFeed")	
+    private BankRateFeed bcbBankRateFeed;
 
 	@GET
 	@Path("/{currencyIn}/{amount}/{currencyOut}")
-	public Long convert(@PathParam("currencyIn") String currencyIn, @PathParam("amount") Long amount, @PathParam("currencyOut") String currencyOut) {
-		return amount; 
+	public Double convert(@PathParam("currencyIn") String currencyIn, @PathParam("amount") Double amount, @PathParam("currencyOut") String currencyOut) throws IllegalArgumentException, BankRateFeedException {
+		//return bcbBankRateFeed.convert(amount, currencyIn, currencyOut);
+		return amount;
 	}
 	
 }
