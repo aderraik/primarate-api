32f5a279ab4a966ad7381fdd297cd099e757b425
Mauricio Ferreira <malf@visiansystems.com>
2017-09-24 22:44:32 +0100

    Added Spring Boot startup project.

---
+++ src/main/java/com/visiansystems/justarate/JustarateApplication.java
@@ -0,0 +1,12 @@
+package com.visiansystems.justarate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustarateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustarateApplication.class, args);
+	}
+}
